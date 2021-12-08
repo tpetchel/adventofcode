31c7c7d57e06eda0d750eb195d5fd364d7b73b57
Thomas Petchel <tpetchel@gmail.com>
2021-12-08 15:14:55 -0800

    Minor tweaks

--- 2021/day1/Program.fs
+++ 2021/day1/Program.fs
@@ -1,11 +1,11 @@
 ﻿// https://adventofcode.com/2021/day/1
 
-// --- Part One ---
+// --- Day 1: Sonar Sweep ---
 let depths = System.IO.File.ReadLines("input.txt") |> Seq.map (fun s -> s |> int) 
-let count_increasing list = list |> Seq.pairwise |> Seq.map (fun (a, b) -> if b > a then 1 else 0) |> Seq.sum
-let count1 = count_increasing depths
+let countIncreasing list = list |> Seq.pairwise |> Seq.map (fun (a, b) -> if b > a then 1 else 0) |> Seq.sum
+let count1 = countIncreasing depths
 printfn "%i" count1
 
 // --- Part Two ---
-let count2 = count_increasing (depths |> Seq.windowed 3 |> Seq.map (fun window -> window |> Seq.sum))
+let count2 = countIncreasing (depths |> Seq.windowed 3 |> Seq.map (fun window -> window |> Seq.sum))
 printfn "%i" count2
