8fda9bf03d0fcef0cc096094becbcf4ab0cdfb6b
Thomas Petchel <tpetchel@gmail.com>
2021-12-26 23:22:34 -0800

    Add day16 project

---
+++ 2021/day16/Program.fs
@@ -0,0 +1,10 @@
+﻿// https://adventofcode.com/2021/day/16
+
+// --- Day 16: Packet Decoder ---
+
+let readInput filename = 
+    System.IO.File.ReadAllLines filename
+
+// --- Part One ---
+
+// --- Part Two ---
