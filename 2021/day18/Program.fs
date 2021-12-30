2d7c92de906b6238c4924352fef7af3c6240d698
Thomas Petchel <tpetchel@gmail.com>
2021-12-30 15:47:26 -0800

    Add day18 project

---
+++ 2021/day18/Program.fs
@@ -0,0 +1,16 @@
+﻿// https://adventofcode.com/2021/day/18
+open System
+
+// --- Day 18: Snailfish ---
+
+// --- Part One ---
+
+// Sample data
+
+// Puzzle data
+
+// --- Part Two ---
+
+// Sample data
+
+// Puzzle data
