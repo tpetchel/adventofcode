9940dfffbea956b571b377504d3be3c215ef375b
Thomas Petchel <tpetchel@gmail.com>
2022-02-01 23:23:33 -0800

    Refactoring

--- 2021/day22/Program.fs
+++ 2021/day22/Program.fs
@@ -114,37 +114,41 @@ let partOne () =
         |> Set.count 
     printfn $"{puzzleOn'}" // 587785
 
-partOne ()
+//partOne ()
 
 // --- Part Two ---
 
+// I really struggled with this one. I put it aside for some time,
+// but still couldn't get it after a break. 
+// This is the second one I needed a hint from (on Reddit.)
+// It takes a posted imperative approach and adapts it to a 
+// (somewhat) more functional style.
+
 let computeSize (cuboid: Cuboid) = 
-    let distance (n: int) (m: int) = 
-        (int64 m) - (int64 n)
+    let distance (n: int) (m: int) = (int64 m) - (int64 n)
     let (x, x') = cuboid.Rx
     let (y, y') = cuboid.Ry
     let (z, z') = cuboid.Rz
     (distance x x') * (distance y y') * (distance z z')
 
-let getOverlapBox (boxA: Cuboid) (boxB: Cuboid) =
-    let xi0a, xi1a = boxA.Rx
-    let yi0a, yi1a = boxA.Ry
-    let zi0a, zi1a = boxA.Rz
-    let xi0b, xi1b = boxB.Rx
-    let yi0b, yi1b = boxB.Ry
-    let zi0b, zi1b = boxB.Rz
+let getOverlap (a: Cuboid) (b: Cuboid) =
+    let xa, xa' = a.Rx
+    let ya, ya' = a.Ry
+    let za, za' = a.Rz
+    let xb, xb' = b.Rx
+    let yb, yb' = b.Ry
+    let zb, zb' = b.Rz
 
-    let x  = max xi0a xi0b
-    let x' = max x (min xi1a xi1b)
-    let y  = max yi0a yi0b
-    let y' = max y (min yi1a yi1b)
-    let z  = max zi0a zi0b
-    let z' = max z (min zi1a zi1b)
+    let x  = max xa xb
+    let x' = max x (min xa' xb')
+    let y  = max ya yb
+    let y' = max y (min ya' yb')
+    let z  = max za zb
+    let z' = max z (min za' zb')
 
     { Rx = (x,x'); Ry = (y,y'); Rz = (z,z'); }
 
-
-let part2 (steps: RebootStep[]) = 
+let reboot2 (steps: RebootStep[]) = 
     let rec calculateCubesToIndex (steps: RebootStep[]) (step: RebootStep) fromIndex =
         if fromIndex >= steps.Length then 0L
         else
@@ -156,35 +160,38 @@ let part2 (steps: RebootStep[]) =
                                     | State.On -> size
                                     | State.Off -> 0L
                                     | _ -> failwith "Invalid state"
-
                 let mutable i = 0
                 while i < fromIndex do
                     let cube2 = steps[i]
-                    let overlapBounds = getOverlapBox step.Bounds cube2.Bounds
+                    let overlapBounds = getOverlap step.Bounds cube2.Bounds
                     let overlapStep = { State = cube2.State; Bounds = overlapBounds; }
                     count <- count - calculateCubesToIndex steps overlapStep i
                     i <- i + 1
                 count
 
+    // For some reason, folks talk about adding one to the second component of each 
+    // cuboid. I don't yet fully understand why, but it works. ;)
     let steps' = steps |> Array.map (fun step ->
         let (x, x') = step.Bounds.Rx
         let (y, y') = step.Bounds.Ry
         let (z, z') = step.Bounds.Rz 
         { State = step.State; Bounds = { Rx = (x,x'+1); Ry = (y,y'+1); Rz = (z,z'+1); } })
 
-    let mutable count = 0L
-    steps' |> Array.iteri (fun i step ->
-        count <- count + calculateCubesToIndex steps' step i)
-    count
+    // Compute the size of each 'on' cuboid and return the sum.
+    steps' 
+    |> Array.mapi (fun i step -> calculateCubesToIndex steps' step i)
+    |> Array.sum
 
+// Sample data
 let sampleCount = 
     System.IO.File.ReadAllLines "sample2.txt"
     |> Array.map parseRebootStep
-    |> part2
+    |> reboot2
 printfn "%i" sampleCount // 2758514936282235
 
+// Puzzle data
 let count = 
     System.IO.File.ReadAllLines "input.txt"
     |> Array.map parseRebootStep
-    |> part2
+    |> reboot2
 printfn "%i" count // 1167985679908143
